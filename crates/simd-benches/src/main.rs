c77ff9dfb526c10a1650a342877893bf8e200f6a
Nugine <nugine@foxmail.com>
2022-07-19 02:39:41 +0800

    simd-benches: base64

--- crates/simd-benches/src/main.rs
+++ crates/simd-benches/src/main.rs
@@ -1,19 +1,19 @@
 use std::time::Instant;
 
-// fn bench_base64(n: usize, src: &str) -> u128 {
-//     let encoded = base64_simd::Base64::STANDARD
-//         .encode_to_boxed_str(src.as_bytes())
-//         .into_boxed_bytes();
+fn bench_base64(n: usize, src: &str) -> u128 {
+    let encoded = base64_simd::Base64::STANDARD
+        .encode_to_boxed_str(src.as_bytes())
+        .into_boxed_bytes();
 
-//     let mut bufs = vec![encoded; n];
+    let mut bufs = vec![encoded; n];
 
-//     let t0 = Instant::now();
-//     for buf in &mut bufs {
-//         let _ = base64_simd::Base64::forgiving_decode_inplace(buf).unwrap();
-//     }
-//     let t1 = Instant::now();
-//     (t1 - t0).as_nanos()
-// }
+    let t0 = Instant::now();
+    for buf in &mut bufs {
+        let _ = base64_simd::Base64::forgiving_decode_inplace(buf).unwrap();
+    }
+    let t1 = Instant::now();
+    (t1 - t0).as_nanos()
+}
 
 fn bench_ascii(n: usize, data: &[u8]) -> u128 {
     let t0 = Instant::now();
@@ -27,26 +27,26 @@ fn bench_ascii(n: usize, data: &[u8]) -> u128 {
 fn main() {
     println!("simd-benches quick mode\n");
 
-    // {
-    //     println!("base64-simd forgiving_decode_inplace");
+    {
+        println!("base64-simd forgiving_decode_inplace");
 
-    //     {
-    //         let src = "helloworld".repeat(100_000);
-    //         let n = 100;
-    //         let time = bench_base64(n, &src);
-    //         let time_per_op = (time / n as u128) as f64 / 1e6;
-    //         println!("long  | n = {n:<8} time = {}ms", time_per_op);
-    //     }
+        {
+            let src = "helloworld".repeat(100_000);
+            let n = 100;
+            let time = bench_base64(n, &src);
+            let time_per_op = (time / n as u128) as f64 / 1e6;
+            println!("long  | n = {n:<8} time = {}ms", time_per_op);
+        }
 
-    //     {
-    //         let src = "123";
-    //         let n = 1_000_000;
-    //         let time = bench_base64(n, src);
-    //         let time_per_op = time / n as u128;
-    //         println!("short | n = {n:<8} time = {}ns", time_per_op);
-    //     }
-    //     println!();
-    // }
+        {
+            let src = "123";
+            let n = 1_000_000;
+            let time = bench_base64(n, src);
+            let time_per_op = time / n as u128;
+            println!("short | n = {n:<8} time = {}ns", time_per_op);
+        }
+        println!();
+    }
 
     {
         println!("is_ascii_ct");
