c1d9fc028c5768cd18698a054de0d4c913e3c15b
Nugine <nugine@foxmail.com>
2022-07-19 02:39:40 +0800

    base64-simd: api

--- crates/base64-simd/src/lib.rs
+++ crates/base64-simd/src/lib.rs
@@ -1,23 +1,20 @@
-// // ! SIMD-accelerated base64 encoding and decoding.
-// // !
-// // ! # Examples
-// // !
-// // ! ```
-// // ! use base64_simd::Base64;
-// // !
-// // ! let bytes = b"hello world";
-// // ! let base64 = Base64::STANDARD;
-// // !
-// // ! let encoded = base64.encode_to_boxed_str(bytes);
-// // ! assert_eq!(&*encoded, "aGVsbG8gd29ybGQ=");
-// // !
-// // ! let decoded = base64.decode_to_boxed_bytes(encoded.as_bytes()).unwrap();
-// // ! assert_eq!(&*decoded, bytes);
-// // ! ```
-// // !
-
-//! TODO
-// TODO:
+//! SIMD-accelerated base64 encoding and decoding.
+//!
+//! # Examples
+//!
+//! ```
+//! use base64_simd::Base64;
+//!
+//! let bytes = b"hello world";
+//! let base64 = Base64::STANDARD;
+//!
+//! let encoded = base64.encode_to_boxed_str(bytes);
+//! assert_eq!(&*encoded, "aGVsbG8gd29ybGQ=");
+//!
+//! let decoded = base64.decode_to_boxed_bytes(encoded.as_bytes()).unwrap();
+//! assert_eq!(&*decoded, bytes);
+//! ```
+//!
 
 #![cfg_attr(not(any(feature = "std", test)), no_std)]
 #![cfg_attr(feature = "unstable", feature(arm_target_feature))]
@@ -42,9 +39,22 @@ mod spec;
 mod decode;
 mod encode;
 
-pub mod multiversion;
+mod multiversion;
+
+pub use simd_abstraction::tools::OutBuf;
 
 // -------------------------------------------------------------------------------------------------
+
+use self::error::ERROR;
+
+use simd_abstraction::item_group;
+use simd_abstraction::tools::slice_mut;
+
+#[cfg(feature = "alloc")]
+item_group!(
+    use alloc::boxed::Box;
+    use simd_abstraction::tools::{alloc_uninit_bytes, assume_init};
+);
 
 #[derive(Debug)]
 enum Base64Kind {
@@ -114,7 +124,7 @@ impl Base64 {
     #[inline]
     pub const fn encoded_length(&self, n: usize) -> usize {
         assert!(n < usize::MAX / 2);
-        unsafe { self::encode::encoded_length_unchecked(n, self.padding) }
+        unsafe { crate::encode::encoded_length_unchecked(n, self.padding) }
     }
 
     /// Estimates the decoded length.
@@ -134,7 +144,132 @@ impl Base64 {
     /// The result is a precise value which can be used for allocation.
     #[inline]
     pub fn decoded_length(&self, data: &[u8]) -> Result<usize, Error> {
-        let (_, m) = self::decode::decoded_length(data, self.padding)?;
+        let (_, m) = crate::decode::decoded_length(data, self.padding)?;
         Ok(m)
+    }
+
+    /// Encodes `src` and writes to `dst`.
+    ///
+    /// # Errors
+    /// This function returns `Err` if:
+    ///
+    /// + The length of `dst` is not enough.
+    #[inline]
+    pub fn encode<'s, 'd>(&'_ self, src: &'s [u8], dst: OutBuf<'d>) -> Result<&'d mut [u8], Error> {
+        unsafe {
+            let m = crate::encode::encoded_length_unchecked(src.len(), self.padding);
+            if dst.len() < m {
+                return Err(ERROR);
+            }
+
+            let mut dst = dst;
+
+            let dst = dst.as_mut_ptr();
+            crate::multiversion::encode_raw::auto_indirect(self, src, dst);
+
+            Ok(slice_mut(dst, m))
+        }
+    }
+
+    /// Decodes `src` and writes to `dst`.
+    ///
+    /// # Errors
+    /// This function returns `Err` if:
+    ///
+    /// + The length of `dst` is not enough.
+    /// + The content of `src` is invalid.
+    #[inline]
+    pub fn decode<'s, 'd>(&'_ self, src: &'s [u8], dst: OutBuf<'d>) -> Result<&'d mut [u8], Error> {
+        unsafe {
+            let (n, m) = crate::decode::decoded_length(src, self.padding)?;
+
+            if dst.len() < m {
+                return Err(ERROR);
+            }
+
+            let mut dst = dst;
+
+            let src: *const u8 = src.as_ptr();
+            let dst: *mut u8 = dst.as_mut_ptr();
+            crate::multiversion::decode_raw::auto_indirect(self, n, m, src, dst)?;
+
+            Ok(slice_mut(dst, m))
+        }
+    }
+
+    /// Decodes `data` and writes inplace.
+    ///
+    /// # Errors
+    /// This function returns `Err` if:
+    ///
+    /// + The content of `data` is invalid.
+    #[inline]
+    pub fn decode_inplace<'d>(&'_ self, data: &'d mut [u8]) -> Result<&'d mut [u8], Error> {
+        unsafe {
+            let (n, m) = crate::decode::decoded_length(data, self.padding)?;
+
+            let dst: *mut u8 = data.as_mut_ptr();
+            let src: *const u8 = dst;
+            crate::multiversion::decode_raw::auto_indirect(self, n, m, src, dst)?;
+
+            Ok(slice_mut(dst, m))
+        }
+    }
+
+    /// Encodes `data` and returns [`Box<str>`]
+    ///
+    /// # Panics
+    /// This function panics if:
+    ///
+    /// + The encoded length of `data` is greater than `isize::MAX`
+    ///
+    #[cfg(feature = "alloc")]
+    #[inline]
+    pub fn encode_to_boxed_str(&self, data: &[u8]) -> Box<str> {
+        if data.is_empty() {
+            return Box::from("");
+        }
+
+        unsafe {
+            let m = crate::encode::encoded_length_unchecked(data.len(), self.padding);
+            assert!(m < usize::MAX / 2);
+
+            let mut uninit_buf = alloc_uninit_bytes(m);
+
+            let dst: *mut u8 = uninit_buf.as_mut_ptr().cast();
+            crate::multiversion::encode_raw::auto_indirect(self, data, dst);
+
+            let len = uninit_buf.len();
+            let ptr = Box::into_raw(uninit_buf).cast::<u8>();
+            Box::from_raw(core::str::from_utf8_unchecked_mut(slice_mut(ptr, len)))
+        }
+    }
+
+    /// Decodes `data` and returns [`Box<[u8]>`](Box)
+    ///
+    /// # Errors
+    /// This function returns `Err` if:
+    ///
+    /// + The content of `data` is invalid.
+    ///
+    #[cfg(feature = "alloc")]
+    #[inline]
+    pub fn decode_to_boxed_bytes(&self, data: &[u8]) -> Result<Box<[u8]>, Error> {
+        if data.is_empty() {
+            return Ok(Box::from([]));
+        }
+
+        unsafe {
+            let (n, m) = crate::decode::decoded_length(data, self.padding)?;
+
+            // safety: 0 < m < isize::MAX
+            let mut uninit_buf = alloc_uninit_bytes(m);
+
+            let dst: *mut u8 = uninit_buf.as_mut_ptr().cast();
+            let src: *const u8 = data.as_ptr();
+            crate::multiversion::decode_raw::auto_indirect(self, n, m, src, dst)?;
+
+            Ok(assume_init(uninit_buf))
+        }
     }
 }
