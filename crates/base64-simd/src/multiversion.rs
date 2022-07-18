aaaf8aaa166d813511a7ac76178e6a0ed7f5a185
Nugine <nugine@foxmail.com>
2022-07-19 02:39:40 +0800

    base64-simd: multiversion

--- crates/base64-simd/src/multiversion.rs
+++ crates/base64-simd/src/multiversion.rs
@@ -1,1 +1,24 @@
 #![allow(missing_docs)]
+
+use crate::decode::{decode_raw_fallback, decode_raw_simd};
+use crate::encode::{encode_raw_fallback, encode_raw_simd};
+use crate::error::Error;
+use crate::Base64;
+
+use simd_abstraction::simd_dispatch;
+
+simd_dispatch!(
+    name        = encode_raw,
+    signature   = fn(base64: &Base64, src: &[u8], dst: *mut u8) -> (),
+    fallback    = encode_raw_fallback,
+    simd        = encode_raw_simd,
+    safety      = {unsafe},
+);
+
+simd_dispatch!(
+    name        = decode_raw,
+    signature   = fn(base64: &Base64, n: usize, m: usize, src: *const u8, dst: *mut u8) -> Result<(), Error>,
+    fallback    = decode_raw_fallback,
+    simd        = decode_raw_simd,
+    safety      = {unsafe},
+);
