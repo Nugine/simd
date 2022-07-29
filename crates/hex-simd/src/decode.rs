22c8801b4b022dcb9d5ae4057cac7342f39e45e2
Nugine <nugine@foxmail.com>
2022-07-29 17:48:20 +0800

    hex-simd: decode: decode_raw_simd

--- crates/hex-simd/src/decode.rs
+++ crates/hex-simd/src/decode.rs
@@ -27,17 +27,17 @@ pub unsafe fn decode_raw_fallback(src: *const u8, len: usize, dst: *mut u8) -> R
 pub unsafe fn decode_raw_simd<S: SIMD256>(
     s: S,
     mut src: *const u8,
-    mut len: usize,
+    len: usize,
     mut dst: *mut u8,
 ) -> Result<(), Error> {
-    while len >= 32 {
-        let chunk = s.v256_load_unaligned(src);
-        let ans = sa_hex::decode_u8x32(s, chunk).map_err(|()| ERROR)?;
-        s.v128_store_unaligned(dst, ans);
+    let end = src.add(len / 32 * 32);
+    while src < end {
+        let x = s.v256_load_unaligned(src);
+        let y = sa_hex::decode_u8x32(s, x).map_err(|()| ERROR)?;
+        s.v128_store_unaligned(dst, y);
         src = src.add(32);
         dst = dst.add(16);
-        len -= 32;
     }
-    decode_raw_fallback(src, len, dst)?;
+    decode_raw_fallback(src, len % 32, dst)?;
     Ok(())
 }
