2df2c7557264e4f4c0f191e440c60edb4b98b827
Nugine <nugine@foxmail.com>
2022-08-03 11:06:19 +0800

    simd-benches: is_utf32le_ct

--- crates/simd-benches/src/main.rs
+++ crates/simd-benches/src/main.rs
@@ -1,5 +1,12 @@
 use std::time::Instant;
 
+fn time(f: impl FnOnce()) -> u128 {
+    let t0 = Instant::now();
+    f();
+    let t1 = Instant::now();
+    (t1 - t0).as_nanos()
+}
+
 fn bench_base64(n: usize, src: &str) -> u128 {
     let encoded = base64_simd::Base64::STANDARD
         .encode_to_boxed_str(src.as_bytes())
@@ -7,21 +14,27 @@ fn bench_base64(n: usize, src: &str) -> u128 {
 
     let mut bufs = vec![encoded; n];
 
-    let t0 = Instant::now();
-    for buf in &mut bufs {
-        let _ = base64_simd::Base64::forgiving_decode_inplace(buf).unwrap();
-    }
-    let t1 = Instant::now();
-    (t1 - t0).as_nanos()
+    time(|| {
+        for buf in &mut bufs {
+            let _ = base64_simd::Base64::forgiving_decode_inplace(buf).unwrap();
+        }
+    })
 }
 
 fn bench_ascii(n: usize, data: &[u8]) -> u128 {
-    let t0 = Instant::now();
-    for _ in 0..n {
-        assert!(unicode_simd::is_ascii_ct(data));
-    }
-    let t1 = Instant::now();
-    (t1 - t0).as_nanos()
+    time(|| {
+        for _ in 0..n {
+            assert!(unicode_simd::is_ascii_ct(data));
+        }
+    })
+}
+
+fn bench_utf32(n: usize, data: &[u32]) -> u128 {
+    time(|| {
+        for _ in 0..n {
+            assert!(unicode_simd::is_utf32le_ct(data));
+        }
+    })
 }
 
 fn main() {
@@ -49,7 +62,7 @@ fn main() {
     }
 
     {
-        println!("is_ascii_ct");
+        println!("unicode-simd is_ascii_ct");
         let data = "helloworld".repeat(100_000);
         let n = 10000;
         let time = bench_ascii(n, data.as_bytes());
@@ -65,5 +78,23 @@ fn main() {
             time_per_op, throughput
         );
         println!();
+    }
+
+    {
+        println!("unicode-simd is_utf32le_ct");
+        let data: Vec<u32> = "helloworld".repeat(100_000).chars().map(|ch| ch as u32).collect();
+        let n = 1000;
+        let time = bench_utf32(n, &data);
+        let time_per_op = (time / n as u128) as f64 / 1e6;
+        let throughput = {
+            let total = n as u128 * data.len() as u128 * 4;
+            let time_sec = time as f64 / 1e9;
+            let gib = u128::pow(1024, 3);
+            total as f64 / gib as f64 / time_sec
+        };
+        println!(
+            "long  | n = {n:<8} time = {}ms, throughput = {:.6}GiB/s",
+            time_per_op, throughput
+        );
     }
 }
