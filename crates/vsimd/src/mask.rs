701b2b3acf32883edf70369e70de93a47305cc7b
Nugine <nugine@foxmail.com>
2022-09-09 16:38:23 +0800

    vsimd: mask

--- crates/vsimd/src/mask.rs
+++ crates/vsimd/src/mask.rs
@@ -1,4 +1,4 @@
-use crate::{AVX2, NEON, SIMD256, SSE41, V256, WASM128};
+use crate::{AVX2, NEON, SIMD128, SIMD256, SSE41, V128, V256, WASM128};
 
 use core::ops::Not;
 
@@ -20,6 +20,19 @@ pub fn mask8x32_all<S: SIMD256>(s: S, x: V256) -> bool {
     }
 }
 
+#[inline(always)]
+pub fn mask8x16_all<S: SIMD128>(s: S, x: V128) -> bool {
+    if is_subtype!(S, SSE41 | WASM128) {
+        return s.u8x16_bitmask(x) == u16::MAX;
+    }
+    if is_subtype!(S, NEON) {
+        return s.u8x16_any_zero(x).not();
+    }
+    {
+        unreachable!()
+    }
+}
+
 /// x: `{{0xff or 0x00}}x32`
 #[inline(always)]
 pub fn mask8x32_any<S: SIMD256>(s: S, x: V256) -> bool {
@@ -32,6 +45,19 @@ pub fn mask8x32_any<S: SIMD256>(s: S, x: V256) -> bool {
     }
     if is_subtype!(S, NEON) {
         return s.v256_all_zero(x).not();
+    }
+    {
+        unreachable!()
+    }
+}
+
+#[inline(always)]
+pub fn mask8x16_any<S: SIMD128>(s: S, x: V128) -> bool {
+    if is_subtype!(S, SSE41 | WASM128) {
+        return s.u8x16_bitmask(x) != 0;
+    }
+    if is_subtype!(S, NEON) {
+        return s.v128_all_zero(x).not();
     }
     {
         unreachable!()
