049826d13f9602100a1dc2da69560d09a09d7059
Nugine <nugine@foxmail.com>
2022-09-24 23:35:31 +0800

    scripts: base64

--- scripts/base64.js
+++ scripts/base64.js
@@ -8,76 +8,74 @@ const now = (() => {
     return () => Number(process.hrtime.bigint());
 })();
 
-function bench(name, n, f) {
+function bench(name, iter, input, f) {
     const t1 = now();
-    for (let i = 0; i < n; ++i) {
-        f(i);
+    for (let i = 0; i < iter; ++i) {
+        f(input);
     }
     const t2 = now();
 
     const dt = (t2 - t1) / 1e9;
-    const freq = n / dt;
-    const time = (t2 - t1) / n;
+    const freq = iter / dt;
+    const time = (t2 - t1) / iter;
 
     const msg = [
-        `${name.padEnd(12)}|`,
-        `n = ${n.toString().padStart(7)},`,
-        `dt = ${dt.toFixed(3).toString().padStart(5)}s,`,
-        `freq = ${freq.toFixed(3).toString().padStart(12)}/s,`,
+        `${name.padEnd(16)}|`,
+        `len = ${input.length.toString().padStart(8)}  |`,
+        `iter = ${iter.toString().padStart(7)}  |`,
+        `dt = ${dt.toFixed(3).toString().padStart(5)}s  |`,
+        `freq = ${freq.toFixed(3).toString().padStart(12)}/s  |`,
         `time = ${time.toFixed(0).toString().padStart(10)}ns/op`
     ];
 
-    console.log(msg.join("    "));
+    console.log(msg.join("  "));
 }
 
-const LONG = "helloworld".repeat(1e5);
-const SHORT = "123";
+const TEST_CASES = [
+    {
+        data: "helloworld".repeat(1e5),
+        iter: 100,
+    },
+    {
+        data: "helloworld".repeat(1e4),
+        iter: 1000,
+    },
+    {
+        data: "helloworld".repeat(1e2),
+        iter: 1e5,
+    },
+    {
+        data: "helloworld".repeat(10),
+        iter: 1e6,
+    },
+    {
+        data: "abcdefghijklmnopqrstuvwx",
+        iter: 1e5,
+    },
+    {
+        data: "123",
+        iter: 1e6
+    }
+]
 
-function b64Long() {
-    const input = LONG;
-    bench("b64Long", 100, () => {
-        atob(btoa(input));
-    });
+const FUNCTIONS = [
+    {
+        name: "encode+decode",
+        call(input) { return atob(btoa(input)); }
+    },
+    {
+        name: "encode",
+        call(input) { return btoa(input); }
+    },
+    {
+        name: "decode",
+        call(input) { return atob(input); }
+    }
+]
+
+for (const t of TEST_CASES) {
+    for (const f of FUNCTIONS) {
+        bench(f.name, t.iter, t.data, f.call);
+    }
+    console.log("    ")
 }
-
-function b64LongE() {
-    const input = LONG;
-    bench("b64LongE", 100, () => {
-        btoa(input);
-    });
-}
-
-function b64LongD() {
-    const input = btoa(LONG);
-    bench("b64LongD", 100, () => {
-        atob(input);
-    });
-}
-
-function b64Short() {
-    const input = SHORT;
-    bench("b64Short", 1e6, () => {
-        atob(btoa(input));
-    });
-}
-
-function b64ShortE() {
-    const input = SHORT;
-    bench("b64ShortE", 1e6, () => {
-        btoa(input);
-    });
-}
-
-function b64ShortD() {
-    const input = btoa(SHORT);
-    bench("b64ShortD", 1e6, () => {
-        atob(input);
-    });
-}
-
-b64Long();
-b64LongE();
-b64LongD();
-b64Short();
-b64ShortE();
-b64ShortD();
