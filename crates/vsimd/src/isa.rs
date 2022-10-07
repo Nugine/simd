fca01066cbd15f7d87555db2c7db6736d3487ec4
Nugine <nugine@foxmail.com>
2022-10-08 01:06:38 +0800

    vsimd: isa: sse2

--- crates/vsimd/src/isa.rs
+++ crates/vsimd/src/isa.rs
@@ -6,16 +6,12 @@ pub unsafe trait InstructionSet: Copy + 'static {
 
     fn is_enabled() -> bool;
 
+    fn is_subtype_of<T: InstructionSet>() -> bool;
+
     #[inline(always)]
     #[must_use]
     fn detect() -> Option<Self> {
         Self::is_enabled().then(|| unsafe { Self::new() })
-    }
-
-    #[inline(always)]
-    #[must_use]
-    fn is_subtype_of<T: InstructionSet>() -> bool {
-        is_same_type::<Self, T>()
     }
 }
 
@@ -49,18 +45,45 @@ macro_rules! is_feature_detected {
     }};
 }
 
+macro_rules! x86_is_enabled {
+    ($feature:tt) => {{
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            is_feature_detected!($feature)
+        }
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        {
+            false
+        }
+    }};
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct SSE2(());
+
+unsafe impl InstructionSet for SSE2 {
+    #[inline(always)]
+    unsafe fn new() -> Self {
+        Self(())
+    }
+
+    #[inline(always)]
+    fn is_enabled() -> bool {
+        x86_is_enabled!("sse2")
+    }
+
+    #[inline(always)]
+    fn is_subtype_of<T: InstructionSet>() -> bool {
+        is_same_type::<Self, T>()
+    }
+}
+
+unsafe impl SIMD64 for SSE2 {}
+// unsafe impl SIMD128 for SSE2 {} // TODO
+// unsafe impl SIMD256 for SSE2 {} // TODO
+
 #[derive(Debug, Clone, Copy)]
 pub struct SSE41(());
-
-#[derive(Debug, Clone, Copy)]
-pub struct AVX2(());
-
-#[allow(clippy::upper_case_acronyms)]
-#[derive(Debug, Clone, Copy)]
-pub struct NEON(());
-
-#[derive(Debug, Clone, Copy)]
-pub struct WASM128(());
 
 unsafe impl InstructionSet for SSE41 {
     #[inline(always)]
@@ -70,20 +93,21 @@ unsafe impl InstructionSet for SSE41 {
 
     #[inline(always)]
     fn is_enabled() -> bool {
-        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-        {
-            is_feature_detected!("sse4.1")
-        }
-        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
-        {
-            false
-        }
+        x86_is_enabled!("sse4.1")
+    }
+
+    #[inline(always)]
+    fn is_subtype_of<T: InstructionSet>() -> bool {
+        is_same_type::<Self, T>() || SSE2::is_subtype_of::<T>()
     }
 }
 
 unsafe impl SIMD64 for SSE41 {}
 unsafe impl SIMD128 for SSE41 {}
 unsafe impl SIMD256 for SSE41 {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct AVX2(());
 
 unsafe impl InstructionSet for AVX2 {
     #[inline(always)]
@@ -93,14 +117,7 @@ unsafe impl InstructionSet for AVX2 {
 
     #[inline(always)]
     fn is_enabled() -> bool {
-        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-        {
-            is_feature_detected!("avx2")
-        }
-        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
-        {
-            false
-        }
+        x86_is_enabled!("avx2")
     }
 
     #[inline(always)]
@@ -112,6 +129,10 @@ unsafe impl InstructionSet for AVX2 {
 unsafe impl SIMD64 for AVX2 {}
 unsafe impl SIMD128 for AVX2 {}
 unsafe impl SIMD256 for AVX2 {}
+
+#[allow(clippy::upper_case_acronyms)]
+#[derive(Debug, Clone, Copy)]
+pub struct NEON(());
 
 unsafe impl InstructionSet for NEON {
     #[inline(always)]
@@ -137,11 +158,19 @@ unsafe impl InstructionSet for NEON {
             false
         }
     }
+
+    #[inline(always)]
+    fn is_subtype_of<T: InstructionSet>() -> bool {
+        is_same_type::<Self, T>()
+    }
 }
 
 unsafe impl SIMD64 for NEON {}
 unsafe impl SIMD128 for NEON {}
 unsafe impl SIMD256 for NEON {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct WASM128(());
 
 unsafe impl InstructionSet for WASM128 {
     #[inline(always)]
@@ -159,6 +188,11 @@ unsafe impl InstructionSet for WASM128 {
         {
             false
         }
+    }
+
+    #[inline(always)]
+    fn is_subtype_of<T: InstructionSet>() -> bool {
+        is_same_type::<Self, T>()
     }
 }
 
