86615929dd6758db09354134a31aaf439bef5261
Nugine <nugine@foxmail.com>
2022-10-08 01:06:38 +0800

    vsimd: mask: sse2

--- crates/vsimd/src/mask.rs
+++ crates/vsimd/src/mask.rs
@@ -1,4 +1,4 @@
-use crate::isa::{AVX2, NEON, SSE41, WASM128};
+use crate::isa::{AVX2, NEON, SSE2, WASM128};
 use crate::vector::{V128, V256};
 use crate::{SIMD128, SIMD256};
 
@@ -7,7 +7,7 @@ use core::ops::Not;
 /// x: `{{0xff or 0x00}}x32`
 #[inline(always)]
 pub fn mask8x16_all<S: SIMD128>(s: S, x: V128) -> bool {
-    if is_subtype!(S, SSE41 | WASM128) {
+    if is_subtype!(S, SSE2 | WASM128) {
         return s.u8x16_bitmask(x) == u16::MAX;
     }
     if is_subtype!(S, NEON) {
@@ -21,7 +21,7 @@ pub fn mask8x32_all<S: SIMD256>(s: S, x: V256) -> bool {
     if is_subtype!(S, AVX2) {
         return s.u8x32_bitmask(x) == u32::MAX;
     }
-    if is_subtype!(S, SSE41 | WASM128 | NEON) {
+    if is_subtype!(S, SSE2 | WASM128 | NEON) {
         let x = x.to_v128x2();
         let x = s.v128_and(x.0, x.1);
         return mask8x16_all(s, x);
@@ -32,7 +32,7 @@ pub fn mask8x32_all<S: SIMD256>(s: S, x: V256) -> bool {
 /// x: `{{0xff or 0x00}}x32`
 #[inline(always)]
 pub fn mask8x16_any<S: SIMD128>(s: S, x: V128) -> bool {
-    if is_subtype!(S, SSE41 | WASM128) {
+    if is_subtype!(S, SSE2 | WASM128) {
         return s.u8x16_bitmask(x) != 0;
     }
     if is_subtype!(S, NEON) {
@@ -46,7 +46,7 @@ pub fn mask8x32_any<S: SIMD256>(s: S, x: V256) -> bool {
     if is_subtype!(S, AVX2) {
         return s.u8x32_bitmask(x) != 0;
     }
-    if is_subtype!(S, SSE41 | WASM128 | NEON) {
+    if is_subtype!(S, SSE2 | WASM128 | NEON) {
         let x = x.to_v128x2();
         let x = s.v128_or(x.0, x.1);
         return mask8x16_any(s, x);
@@ -56,7 +56,7 @@ pub fn mask8x32_any<S: SIMD256>(s: S, x: V256) -> bool {
 
 #[inline(always)]
 pub fn u8x16_highbit_all<S: SIMD128>(s: S, x: V128) -> bool {
-    if is_subtype!(S, SSE41 | WASM128) {
+    if is_subtype!(S, SSE2 | WASM128) {
         return s.u8x16_bitmask(x) == u16::MAX;
     }
     if is_subtype!(S, NEON) {
@@ -75,7 +75,7 @@ pub fn u8x32_highbit_all<S: SIMD256>(s: S, x: V256) -> bool {
     if is_subtype!(S, AVX2) {
         return s.u8x32_bitmask(x) == u32::MAX;
     }
-    if is_subtype!(S, SSE41 | WASM128 | NEON) {
+    if is_subtype!(S, SSE2 | WASM128 | NEON) {
         let x = x.to_v128x2();
         let x = s.v128_and(x.0, x.1);
         return u8x16_highbit_all(s, x);
@@ -85,7 +85,7 @@ pub fn u8x32_highbit_all<S: SIMD256>(s: S, x: V256) -> bool {
 
 #[inline(always)]
 pub fn u8x16_highbit_any<S: SIMD128>(s: S, x: V128) -> bool {
-    if is_subtype!(S, SSE41 | WASM128) {
+    if is_subtype!(S, SSE2 | WASM128) {
         return s.u8x16_bitmask(x) != 0;
     }
     if is_subtype!(S, NEON) {
@@ -104,7 +104,7 @@ pub fn u8x32_highbit_any<S: SIMD256>(s: S, x: V256) -> bool {
     if is_subtype!(S, AVX2) {
         return s.u8x32_bitmask(x) != 0;
     }
-    if is_subtype!(S, SSE41 | WASM128 | NEON) {
+    if is_subtype!(S, SSE2 | WASM128 | NEON) {
         let x = x.to_v128x2();
         let x = s.v128_or(x.0, x.1);
         return u8x16_highbit_any(s, x);
